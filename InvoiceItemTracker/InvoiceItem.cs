4d18f44e5d485220702c66866952097037a693fe
Thomas Waters <Thomas.waters05@gmail.com>
2017-02-13 17:48:34 -0800

    pushing to github

---
+++ InvoiceItemTracker/InvoiceItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceItemTracker
+{
+    public class InvoiceItem
+    {
+        public string Buyer { get; set; } //THESE ARE PROPERTIES
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public InvoiceItem() //THIS IS A CONSTRUCTOR    
+        {
+            this.Timestamp =  DateTime.Now;
+        }
+
+        public InvoiceItem(DateTime timestamp)
+        {
+            this.Timestamp = timestamp;
+        }
+
+        public bool IsOverdue()
+        {
+            TimeSpan difference = DateTime.Now - this.Timestamp;
+            return (difference.Minutes > 4); //you could use an IF stmt but this is far better
+
+        }
+    }
+}
---
+++ InvoiceItemTrackerTests/InvoiceItemTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InvoiceItemTracker;
+
+namespace InvoiceItemTrackerTests
+{
+    [TestClass]
+    public class InvoiceItemTests
+    {
+        [TestMethod]
+        public void TestCreatingAnItem()
+        {
+
+            //Arrange
+            var item = new InvoiceItem();
+            //Assert
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void TestOverdue1()
+        {
+            //Arrange
+            var item = new InvoiceItem();
+            //Assert
+            Assert.IsFalse(item.IsOverdue()); //IsOverdue() is a method so we must call it
+        }
+
+        [TestMethod]
+        public void TestOverdue2()
+        {
+            //Arrange
+            var now = DateTime.Now;
+            var then = now.AddMinutes(-3).AddSeconds(-59);
+            var item = new InvoiceItem(then);
+            //Assert
+            Assert.IsFalse(item.IsOverdue());
+        }
+
+        [TestMethod]
+        public void TestOverdue3()
+        {
+            //Arrange
+            var fiveMinutes = new TimeSpan(0, 5, 0);
+            var fiveMinutesAgo = DateTime.Now.Subtract(fiveMinutes);
+            var item = new InvoiceItem(fiveMinutesAgo);
+
+            //Assert
+            Assert.IsTrue(item.IsOverdue());
+
+        }
+    }
+}
